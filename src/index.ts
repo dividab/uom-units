132e25954e34874d65479b5e1401720caab8e5b1
Jonathan Mourtada <jonathan.mourtada@divid.se>
2019-11-05 14:57:34 +0100

    export quantity

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import * as Units from "./units";
 import * as UnitsFormat from "./units-format";
 import * as UnitDivide from "./unit-divide";
 import * as UnitTimes from "./unit-times";
+import * as Quantity from "./quantity";
 
-export { Units, UnitsFormat, UnitDivide, UnitTimes };
+export { Units, UnitsFormat, UnitDivide, UnitTimes, Quantity };
