c4a7e76d0661435ffba0adb44637cfdb8ad48a6e
Johan Kåhlman <johan.kahlman@divid.se>
2022-03-21 14:13:03 +0100

    new-emission-unit-formats

--- src/units-format/emission-format.ts
+++ src/units-format/emission-format.ts
@@ -2,4 +2,11 @@ import { UnitFormat } from "uom";
 
 export const KilogramPerKiloWattHour = UnitFormat.createUnitFormat("kg/kWh", 2);
 export const GramPerKiloWattHour = UnitFormat.createUnitFormat("g/kWh", 1);
+export const GrainPerKiloWattHour = UnitFormat.createUnitFormat("gr/kWh", 1);
 export const PoundLbPerKiloWattHour = UnitFormat.createUnitFormat("lb/kWh", 2);
+export const GramPerKiloJoule = UnitFormat.createUnitFormat("g/KJ", 1);
+export const GramPerMegaJoule = UnitFormat.createUnitFormat("g/MJ", 1);
+export const GrainPerKiloBtu = UnitFormat.createUnitFormat("gr/KBTU", 1);
+export const GrainPerMegaBtu = UnitFormat.createUnitFormat("gr/MBTU", 1);
+export const PoundLbPerKiloBtu = UnitFormat.createUnitFormat("lb/KBTU", 2);
+export const PoundLbPerMegaBtu = UnitFormat.createUnitFormat("lb/MBTU", 2);
