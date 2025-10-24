5a35e5042121f51a878c6924ca39cf7afa7b5383
Tim Svensson <tim.svensson@divid.se>
2025-10-24 12:02:43 +0200

    more performant unitLookup

--- src/unit-lookup/index.ts
+++ src/unit-lookup/index.ts
@@ -1,16 +1,19 @@
-import { UnitMap } from "uom";
+import { Unit, UnitMap } from "uom";
 import * as Units from "../units";
 
-const unitsLowerCase = Object.keys(Units).reduce<UnitMap.UnitMap>(
-  (soFar, current) => {
-    return {
-      ...soFar,
-      [current.toLowerCase()]: (Units as UnitMap.UnitMap)[current],
-    };
-  },
-  {}
-);
-export const unitLookup: UnitMap.UnitLookup = (unitString) =>
-  (unitString &&
-    (unitsLowerCase as UnitMap.UnitMap)[unitString.toLowerCase()]) ||
-  undefined;
+export type UnitName = keyof typeof Units;
+export type Quantity = typeof Units[keyof typeof Units]["quantity"];
+
+const lowerCase: Record<string, Unit.Unit<unknown>> = {};
+const upperCase: Record<string, Unit.Unit<unknown>> = {};
+for (const key of Object.keys(Units)) {
+  lowerCase[key.toLowerCase()] = (Units as UnitMap.UnitMap)[key];
+  upperCase[key] = (Units as UnitMap.UnitMap)[key];
+}
+
+export const unitLookup = <TQuantityName>(
+  unitString: string
+): Unit.Unit<TQuantityName> | undefined =>
+  (upperCase[unitString] ?? lowerCase[unitString.toLowerCase()]) as
+    | Unit.Unit<TQuantityName>
+    | undefined;
